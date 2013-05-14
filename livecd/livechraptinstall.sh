4e0557e2a53fb7f8bf1e76e6f122d68f48847f77
Daniel 'yp2' Derezinski <daniel.derezinski@gmail.com>
2013-05-14 22:46:39 +0200

    Usuniecie aktualizacji systemu podczas budowy

--- livecd/livechraptinstall.sh
+++ livecd/livechraptinstall.sh
@@ -6,5 +6,5 @@ export LC_ALL=C
 # instalacja pakietow z pliku bez pakietow zalecanych
 echo "instalacja pakitow ..."
 sudo apt-get update
-sudo apt-get upgrade
+#sudo apt-get upgrade
 cat /root/inst_deb_wseix.txt | xargs apt-get install --no-install-recommends -y
